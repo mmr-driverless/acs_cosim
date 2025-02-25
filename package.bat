41dcace99de4db5c4f3ee5f1e899c3f351e6e965
Shibodd <simonebondi456@gmail.com>
2025-02-25 14:34:09 +0100

    Packaging

---
+++ package.bat
@@ -0,0 +1,9 @@
+cd MMR_Simulator_DLL\x64\Release\
+7z u ..\..\..\package.7z MMR_Simulator_DLL.dll
+cd ..\..\..\
+
+cd Launcher\x64\Release\
+7z u ..\..\..\package.7z Launcher.exe
+cd ..\..\..\
+
+pause
