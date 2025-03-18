59c7388f9762fa1564ff45dca938b5ea5db42c7f
Marcello Garonzi <marce.gnz303@gmail.com>
2025-03-18 15:35:04 +0100

    Bypassed simulation speed limit

--- MMR_Simulator_DLL/dllmain.cpp
+++ MMR_Simulator_DLL/dllmain.cpp
@@ -5,6 +5,7 @@
 #include "Car.hpp"
 #include "CarAvatar.hpp"
 #include "UDPCommandListener.hpp"
+#include "PhysicsDriveThread.hpp"
 #include <iostream>
 #include <acs_cosim/interface/server.hpp>
 #include <acs_cosim/interface/constants.hpp>
@@ -23,6 +24,7 @@ using namespace acs_cosim::interface::constants;
 
 AcsServer acs_server("tcp://*:5555");
 UDPCommandListener* commandListener = nullptr;
+PhysicsDriveThread* physicsDriveThread = nullptr;
 
 // Hooked functions
 typedef void (*getCarPhysicsStateT)(Car*, CarPhysicsState*);
@@ -43,6 +45,9 @@ simGetCarT originalSimGetCarFunction;
 typedef void (*goToSpawnPositionT)(void*, std::wstring);
 goToSpawnPositionT originalGoToSpawnPosition;
 
+typedef void (*physicsDriveThreadRunT)(PhysicsDriveThread*);
+physicsDriveThreadRunT originalPhysicsDriveThreadRun;
+
 static Parameters params;
 
 bool firstStep = true;
@@ -53,6 +58,19 @@ void sendSimulationStateMsg(Car* car) {
 	SimulationState simulationState(car->ksPhysics->physicsTime, cps);
 	SimulationStateMsg simulationStateMsg(simulationState);
 	acs_server.send_message(&simulationStateMsg);
+}
+
+/**
+ * The only purpose of this function is to find the address of PhysicsDriveThread
+ */
+void hookedPhysicsDriveThreadRun(PhysicsDriveThread* pdt) {
+	if (physicsDriveThread == nullptr) {
+		physicsDriveThread = pdt;
+	}
+
+	if (originalPhysicsDriveThreadRun) {
+		originalPhysicsDriveThreadRun(pdt);
+	}
 }
 
 /**
@@ -121,6 +139,10 @@ void hookedCarPollControls(Car* car, float period) {
 			break;
 		}
 	} while (!advance);
+
+	if (physicsDriveThread) {
+		physicsDriveThread->currentTime = car->ksPhysics->gameTime;
+	}
 }
 
 void dll_attached(HMODULE hModule) {
@@ -187,12 +209,21 @@ void dll_attached(HMODULE hModule) {
 		std::cerr << "Failed to find function CarAvatar::goToSpawnPosition" << std::endl;
 	}
 
+	void* physicsDriveThreadRun = DetourFindFunction(moduleName.c_str(), "PhysicsDriveThread::run");
+	if (physicsDriveThreadRun) {
+		originalPhysicsDriveThreadRun = (physicsDriveThreadRunT)physicsDriveThreadRun;
+	}
+	else {
+		std::cerr << "Failed to find function PhysicsDriveThread::run" << std::endl;
+	}
+
 	// Hooking delle funzioni
 	if (acquireControlsAddress) {
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
 		DetourAttach(&(PVOID&)originalCarPollControlsFunction, hookedCarPollControls);
 		DetourAttach(&(PVOID&)originalCommandListenerUpdateFunction, hookedCommandListenerUpdateFunction);
+		DetourAttach(&(PVOID&)originalPhysicsDriveThreadRun, hookedPhysicsDriveThreadRun);
 		DetourTransactionCommit();
 	}
 }
