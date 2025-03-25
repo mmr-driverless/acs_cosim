c9b2e012864ffcf88ff35f0d927ebf2851c7697f
Shibodd <simonebondi456@gmail.com>
2025-03-25 21:51:38 +0100

    Remove everything Visual Studio related

--- package.bat
+++
@@ -1,9 +0,0 @@
-cd MMR_Simulator_DLL\x64\Release\
-7z u ..\..\..\package.7z MMR_Simulator_DLL.dll
-cd ..\..\..\
-
-cd Launcher\x64\Release\
-7z u ..\..\..\package.7z Launcher.exe
-cd ..\..\..\
-
-pause
